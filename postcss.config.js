a3c816d182c69d1d1abe9e9a4d6db89ca97d9791
ulfschneider <u@ulf.codes>
2020-06-21 13:39:54 +0200

    Improve purge-css extractor

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,25 @@
+
+
 module.exports = {
   plugins: [
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-discard-comments'),
     require('cssnano')({
       preset: 'default',
+    }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./**/*.html', './**/*.md', './**/*.njk'],
+      defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+      }
     })
+
   ]
 }
