d3d242f91c100fb30ddb49afd871eb2110697b15
ulfschneider <u@ulf.codes>
2020-07-11 12:46:53 +0200

    Scaling down images

--- _data/site.js
+++ _data/site.js
@@ -4,6 +4,8 @@ module.exports = {
     locale: "en",
     themecolor: "#00bbff",
     url: "https://ulf.codes",
+    imgMaxWidth: 600,
+    newer: true,
     title: "Ulf Schneider – Developer and Agile Coach",
     description: "Hi, I´m Ulf Schneider, a developer and agile coach living in Paderborn, Germany.",
     author: {
