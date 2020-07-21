a3cff5fe23852e0ce7f0f980d7365f3accf00944
ulfschneider <u@ulf.codes>
2020-07-21 10:23:49 +0200

    Reorganize gulp processing

--- _data/site.js
+++ _data/site.js
@@ -4,8 +4,7 @@ module.exports = {
     locale: "en",
     themecolor: "#00bbff",
     url: "https://ulf.codes",
-    imgMaxWidth: 600,
-    newer: true,
+    imgMaxWidth: 600,    
     title: "Ulf Schneider – Developer and Agile Coach",
     description: "Hi, I´m Ulf Schneider, a developer and agile coach living in Paderborn, Germany.",
     author: {
--- _gulp/images.js
+++ _gulp/images.js
@@ -1,18 +1,15 @@
 const { dest, src } = require('gulp');
-const newer = require('gulp-newer');
 const gulpif = require('gulp-if');
 
 const through = require('through2');
 const path = require('path')
 const scaleImages = require('gulp-scale-images');
-const imagemin = require('gulp-imagemin');
 
 const SOURCE = 'content/img/**/*';
 const DEST = '_site/img/';
 
 const site = require('../_data/site.js');
 const MAX_WIDTH = site.imgMaxWidth;
-const NEWER = site.newer == undefined ? true : site.newer;
 
 const computeScaleInstructions = (file, _, cb) => {
     file.scale = {
@@ -26,34 +23,17 @@ const computeFileName = (output, scale, cb) => {
     cb(null, fileName)
 }
 
-const images = () => {
-    if (!NEWER) {
-        console.log('newer is configured to be false in site.js, therefore optimizing all images');
-    } else {
-        console.log('Optimizing only images where the source is newer than the destination');
-    }
+const processingImages = () => {
     if (MAX_WIDTH <= 0) {
-        console.log('No imgMaxWidth set in site.js, therefore not scaling down images');
+        console.warn('No imgMaxWidth set in site.js, therefore not scaling down images');
     }
-    
+
     return src(SOURCE)
-        .pipe(gulpif(NEWER, newer(DEST)))
         .pipe(gulpif(MAX_WIDTH > 0,
             through.obj(computeScaleInstructions)))
         .pipe(gulpif(MAX_WIDTH > 0,
             scaleImages(computeFileName)))
-        .pipe(
-            imagemin(
-                [
-                    imagemin.gifsicle({ interlaced: true }),
-                    imagemin.mozjpeg({ quality: 85, progressive: true }),
-                    imagemin.optipng({ optimizationLevel: 5, interlaced: null })
-                ], {
-                verbose: true
-            }
-            )
-        )
         .pipe(dest(DEST));
 };
 
-module.exports = images;
+module.exports = processingImages;
