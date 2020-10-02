bac23bdd9aa96ba873347cf4598497c15037c48a
ulfschneider <u@ulf.codes>
2020-10-02 08:47:37 +0200

    Update 11ty

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ const javascript = require('./_gulp/javascript.js');
 const { series, watch } = require('gulp');
 
 const watcher = () => {
-    watch(['_data/site.js', 'content/img/**/*'], { ignoreInitial: true }, images);
     watch(['_assets/css/**/*', 'content/**/*', '_includes/**/*', '_layouts/**/*'], { ignoreInitial: true }, css);
     watch(['_assets/js/**/*'], { ignoreInitial: true }, javascript);
+    watch(['_data/site.js', 'content/img/**/*'], { ignoreInitial: true }, images);
 };
 
-exports.default = series([javascript, images, css]);
+exports.default = series([javascript, css, images]);
 
 exports.watch = watcher;
