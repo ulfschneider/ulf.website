8dec478131a33031eddcffe898da362150923033
ulfschneider <u@ulf.codes>
2020-10-24 21:24:55 +0200

    Use purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,19 @@
 module.exports = {
     plugins: [
+        require('postcss-import'),
         require('postcss-custom-media'),
         require('postcss-calc'),
         require('autoprefixer'),
-        require('postcss-import'),
         require('postcss-discard-comments'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                'content/**/*',
+                '!content/**/*compose.html',
+                '_includes/**/*',
+                '_layouts/**/*',
+                '_assets/js/**/*'
+            ]
+        }),
         require('cssnano')
     ]
 }
