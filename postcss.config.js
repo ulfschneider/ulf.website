b8d4556b16741ec7f1639699515dd6e73017bcf4
ulfschneider <u@ulf.codes>
2020-10-24 15:45:08 +0200

    Fix build pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('postcss-custom-media'),
-        require('postcss-custom-properties'),
         require('postcss-calc'),
         require('autoprefixer'),
         require('postcss-import'),
