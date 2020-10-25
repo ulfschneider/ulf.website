570a2612130c25c6783b9dfba1a965ff5354028c
ulfschneider <u@ulf.codes>
2020-10-25 09:22:43 +0100

    More accurate purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,15 @@ module.exports = {
                 '_includes/**/*',
                 '_layouts/**/*',
                 '_assets/js/**/*'
-            ]
+            ],
+
+            defaultExtractor: content => {
+                // Capture as liberally as possible, including things like `h-(screen-1.5)`
+                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+                    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+                return broadMatches.concat(innerMatches)
+            }
         }),
         require('cssnano')
     ]
