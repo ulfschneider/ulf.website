5ae4513cc83ab06ecefd918dbbc3dc4790cdcc16
ulfschneider <u@ulf.codes>
2020-11-14 14:32:46 +0100

    PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-discard-comments'),
+    require('postcss-purgecss')({
+      content: ['./_site/**/*.html', './content/**/*.html']
+    }),    
     require('cssnano')({
       preset: 'default',
     })
