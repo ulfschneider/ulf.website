39ce6b41c3144dd392c2c1afd6a2368a6378bba3
ulfschneider <u@ulf.codes>
2020-11-30 19:20:48 +0100

    New list style

--- _root/serviceworker.js
+++ _root/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v22'; //version is used to remove old caches
+const CACHE_VERSION = 'v23'; //version is used to remove old caches
 
 const SCRIPT = 'script';
 const RUNTIME = 'runtime';
