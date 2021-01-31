5b70e9586ab0d9e8c56976dd4dd4c606f34b1429
ulfschneider <u@ulf.codes>
2021-01-31 14:37:50 +0100

    Adjust gallery

--- _root/serviceworker.js
+++ _root/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v31'; //version is used to remove old caches
+const CACHE_VERSION = 'v32'; //version is used to remove old caches
 
 const SCRIPT = 'script';
 const RUNTIME = 'runtime';
