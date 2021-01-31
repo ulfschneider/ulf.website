a724c5104b69b52c22d13ba358ab692996713004
ulfschneider <u@ulf.codes>
2021-01-31 14:49:48 +0100

    Latest compose

--- _root/serviceworker.js
+++ _root/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v32'; //version is used to remove old caches
+const CACHE_VERSION = 'v33'; //version is used to remove old caches
 
 const SCRIPT = 'script';
 const RUNTIME = 'runtime';
