422e7b49b15f3e3bed5d081156ac7d5c333e876a
ulfschneider <u@ulf.codes>
2021-04-25 15:50:01 +0200

    Repair blog appearance

--- _eleventy/utils.js
+++ _eleventy/utils.js
@@ -90,7 +90,7 @@ module.exports = {
 
     isLiveItem: function(item) {
         const now = new Date();
-        return !item.date || item.date <= now &&
+        return !item.date ||  item.date <= now &&
             item.data.draft !== true &&
             item.data.draft !== 'yes';
     },
@@ -143,8 +143,6 @@ module.exports = {
                     return true;
                 }
             }
-        } else {
-            return true;
         }
         return false;
     },
