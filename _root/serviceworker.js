081a887bc0b8d992a56b96abac03a224b16430b9
ulfschneider <u@ulf.codes>
2021-04-27 16:32:52 +0200

    Keep old service worker

--- _root/serviceworker.js
+++ _root/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v36'; //version is used to remove old caches
+const CACHE_VERSION = 'v35'; //version is used to remove old caches
 
 const SCRIPT = 'script';
 const RUNTIME = 'runtime';
