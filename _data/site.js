91a8acfd38aa60c1b3f96b671ce278d3ff6831d4
ulfschneider <mail@ulf.codes>
2022-12-13 17:50:26 +0100

    Smaller 3xl font

--- _data/site.js
+++ _data/site.js
@@ -7,7 +7,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-0`,
-        css: `${CACHE_VERSION}-10`,
+        css: `${CACHE_VERSION}-11`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-0`,
         image: `${CACHE_VERSION}-0`,
