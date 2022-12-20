b539471c7c5e507010d9b43c5fb1e6a3f0540cd7
ulfschneider <mail@ulf.codes>
2022-12-20 07:40:07 +0100

    Switch back to previous contrast for dark mode

--- _data/site.js
+++ _data/site.js
@@ -7,7 +7,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-0`,
-        css: `${CACHE_VERSION}-21`,
+        css: `${CACHE_VERSION}-22`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-0`,
         image: `${CACHE_VERSION}-0`,
