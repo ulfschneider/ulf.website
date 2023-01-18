bacd6ff73d2f8f32b0575d382113138b32c12f0e
ulfschneider <mail@ulf.codes>
2023-01-18 07:03:36 +0100

    Overflow-x-auto for pre code

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-7`,
-        css: `${CACHE_VERSION}-48`,
+        css: `${CACHE_VERSION}-49`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-1`,
         image: `${CACHE_VERSION}-0`,
