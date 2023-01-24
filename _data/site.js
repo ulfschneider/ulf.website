d7de003bf71176638880218710ae26a51aa564dd
ulfschneider <mail@ulf.codes>
2023-01-24 13:26:21 +0100

    Masonry layout for the gallery (for Firefox behind feature flag)

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-7`,
-        css: `${CACHE_VERSION}-50`,
+        css: `${CACHE_VERSION}-51`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-1`,
         image: `${CACHE_VERSION}-0`,
