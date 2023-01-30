0d6cbba38d1453a4420729eded4257818e82c81a
ulfschneider <mail@ulf.codes>
2023-01-30 12:53:37 +0100

    Bolder titles

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-7`,
-        css: `${CACHE_VERSION}-54`,
+        css: `${CACHE_VERSION}-55`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
