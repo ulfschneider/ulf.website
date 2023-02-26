01b3421b833c31b32098d05870c1f8d25d72fa15
ulfschneider <mail@ulf.codes>
2023-02-26 20:15:58 +0100

    Centered layout

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-9`,
-        css: `${CACHE_VERSION}-56`,
+        css: `${CACHE_VERSION}-57`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
