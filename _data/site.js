5e583af9334fcb36f38166cac2f523d8d922f84e
ulfschneider <mail@ulf.codes>
2023-02-27 18:11:33 +0100

    Styling iframes

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-9`,
-        css: `${CACHE_VERSION}-57`,
+        css: `${CACHE_VERSION}-58`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
