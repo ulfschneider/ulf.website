f21ba7918240cb74400b2b9315301c47672dea36
ulfschneider <mail@ulf.codes>
2023-03-04 19:11:40 +0100

    Being able to create issues

--- _edge/comments.js
+++ _edge/comments.js
@@ -8,11 +8,14 @@ const LABEL_FILTER = 'website-comments'; //use empty string to ignore label filt
 let octokit;
 let issues;
 let comments;
+let commentRoot;
+let commentRootIssue;
+let commentRootNumber;
 
 
 async function loginGitHub() {
     // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
-    const token = config().GITHUB_PAT || process.env.GITHUB_PAT;
+    const token = config().GITHUB_PAT || Deno.env.get(GITHUB_PAT);
     return new Octokit({ auth: token });
 }
 
@@ -24,7 +27,8 @@ async function getRemainingRateLimit() {
 
 async function loadIssues() {
     issues = [];
-    return octokit.paginate(octokit.rest.issues.listForRepo, {
+    comments = [];
+    await octokit.paginate(octokit.rest.issues.listForRepo, {
         owner: OWNER,
         repo: REPO,
         labels: LABEL_FILTER
@@ -33,27 +37,52 @@ async function loadIssues() {
             //paginate returns all issues of the repo in an array
             issues = allIssues;
         });
-    return issues;
+    getCommentRootIssue();
 }
 
 
-function getIssueByTitle(title) {
+function getCommentRootIssue() {
+    commentRootNumber = -1;
+    commentRootIssue = null;
     if (issues) {
         for (let issue of issues) {
-            if (issue.title == title) {
-                return issue;
+            if (issue.title == commentRoot) {
+                commentRootIssue = issue;
+                commentRootNumber = issue.number;
+                console.log(`${commentRoot} has the url ${commentRootIssue.html_url}`);
+                return commentRootIssue;
             }
         }
     }
     return null;
 }
 
-async function loadComments(commentRoot) {
+async function createCommentRootIssue() {
+    await octokit.rest.issues.create({
+        owner: OWNER,
+        repo: REPO,
+        title: commentRoot,
+        labels: [LABEL_FILTER]
+    });
+    //TODO store a backlink to the original website post
+}
+
+async function createComment(commentBody) {
+    if (commentRootNumber < 0) {
+        await createCommentRootIssue();
+        await loadIssues();
+    }
+    await octokit.rest.issues.createComment({
+        owner: OWNER,
+        repo: REPO,
+        issue_number: commentRootNumber,
+        body: commentBody,
+    });
+}
+
+async function loadComments() {
     comments = [];
-    let commentRootIssue = getIssueByTitle(commentRoot);
-    if (!commentRootIssue) {
-        console.log(`No comment root ${commentRoot} found`);
-    } else {
+    if (commentRootIssue) {
         const { data } = await octokit.rest.issues.listComments({
             owner: OWNER,
             repo: REPO,
@@ -70,6 +99,23 @@ async function loadComments(commentRoot) {
 
 export default async (request, context) => {
 
+    const url = new URL(request.url);
+    const searchParams = url.searchParams;
+    const method = request.method;
+    const body = request.body;
+    commentRoot = searchParams.get('root');
+
+    console.log(method, commentRoot);
+
+    if (!commentRoot) {
+        console.error('The comment root argument is not specified');
+        return new Response(JSON.stringify('You didn´t specify a comment root with ?root='), {
+            status: 400,
+            headers: { "content-type": "application/json;charset=UTF-8" }
+        });
+
+    }
+
     if (!octokit) {
         octokit = await loginGitHub();
     }
@@ -84,17 +130,13 @@ export default async (request, context) => {
         };
     }
 
-    const url = new URL(request.url);
-    const searchParams = url.searchParams;
-    const commentRoot = searchParams.get('comment-root');
 
     await loadIssues();
-    await loadComments(commentRoot);
-
-    for (let comment of comments) {
-        console.log(comment.body);
+    await createComment('my new body');
+    await loadComments();
+    for (let c of comments) {
+        console.log(c.body);
     }
-
 
     return new Response(JSON.stringify(commentRoot), {
         status: 200,
