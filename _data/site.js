f7fd085370dd93e1f0b71e04dff65124269d07a2
ulfschneider <mail@ulf.codes>
2023-03-12 13:42:37 +0100

    Refresh CSS and Script cache

--- _data/site.js
+++ _data/site.js
@@ -7,8 +7,8 @@ const CACHE_VERSION = 'v1';
 module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
-        script: `${CACHE_VERSION}-9`,
-        css: `${CACHE_VERSION}-62`,
+        script: `${CACHE_VERSION}-10`,
+        css: `${CACHE_VERSION}-63`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
