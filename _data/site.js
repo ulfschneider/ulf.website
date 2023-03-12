8f5c6fd63f36fb5ce6fe3f1e4004a10b60da2057
ulfschneider <mail@ulf.codes>
2023-03-12 20:13:46 +0100

    Different image filter for dark mode

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-11`,
-        css: `${CACHE_VERSION}-64`,
+        css: `${CACHE_VERSION}-65`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
