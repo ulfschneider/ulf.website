e357e1f92acca659341ab1a955646887d8e72125
ulfschneider <mail@ulf.codes>
2023-03-18 08:59:22 +0100

    Move site-tags after list content

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-12`,
-        css: `${CACHE_VERSION}-66`,
+        css: `${CACHE_VERSION}-67`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
