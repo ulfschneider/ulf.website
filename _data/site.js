13d0b7d9f638045fbce16684b69042036dc7c94c
ulfschneider <mail@ulf.codes>
2023-03-27 08:13:55 +0200

    Onest stylistic sets ss01 and ss02

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
     cacheVersioning: {
         runtime: `${CACHE_VERSION}-0`,
         script: `${CACHE_VERSION}-12`,
-        css: `${CACHE_VERSION}-68`,
+        css: `${CACHE_VERSION}-69`,
         json: `${CACHE_VERSION}-0`,
         search: `${CACHE_VERSION}-2`,
         image: `${CACHE_VERSION}-0`,
