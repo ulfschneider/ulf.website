fb19263cfe888f97b7b7d72c70427aa5d5b81b59
ulfschneider <mail@ulf.codes>
2023-06-29 07:49:00 +0200

    Setting vert rhythm to xl

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-20`,
-    css: `${CACHE_VERSION}-85`,
+    css: `${CACHE_VERSION}-86`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
