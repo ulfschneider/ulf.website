ad823cda608aa008604d492569f25d46bf22a146
ulfschneider <mail@ulfschneider.io>
2023-10-08 08:25:08 +0200

    Clear css cache

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-20`,
-    css: `${CACHE_VERSION}-88`,
+    css: `${CACHE_VERSION}-89`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
