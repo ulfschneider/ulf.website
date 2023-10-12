c627bfa212de8eb1284f4d7e9c7f6c3b8efe175d
ulfschneider <mail@ulfschneider.io>
2023-10-12 08:13:49 +0200

    Refresh cache

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-91`,
+    css: `${CACHE_VERSION}-92`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
