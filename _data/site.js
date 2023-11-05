b3725a866dff9a89adf8e60ebba215878b0e510e
ulfschneider <mail@ulfschneider.io>
2023-11-05 06:31:29 +0100

    New text-balance utility class, abstract is balanced

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,8 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-98`,
+    css: `${CACHE_VERSION}-9
+    98`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
