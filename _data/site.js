9811ab43fc60a959f36c6d4d9ebc7e19bd548c7a
ulfschneider <mail@ulfschneider.io>
2023-11-05 08:18:20 +0100

    Reduce whitespace around comments and post navigation

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-99`,
+    css: `${CACHE_VERSION}-100`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
