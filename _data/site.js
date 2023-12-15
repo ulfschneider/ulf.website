1969a9d74b8e3b71f5cd3bcf12e615322a188f6e
ulfschneider <mail@ulfschneider.io>
2023-12-15 10:48:45 +0100

    Adjust leading and align site tags with tag intro

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-123`,
+    css: `${CACHE_VERSION}-124`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
