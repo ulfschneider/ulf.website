ef35993c80bac4ccd993b30dba6e4680f07f4ae7
ulfschneider <mail@ulfschneider.io>
2023-12-16 08:58:56 +0100

    Font-sizes, leading, and tracking

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-126`,
+    css: `${CACHE_VERSION}-127`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
