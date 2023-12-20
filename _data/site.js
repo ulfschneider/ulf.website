933e8e855cebb2c4acff58a43eb0f4438be44a4b
ulfschneider <mail@ulfschneider.io>
2023-12-20 14:00:39 +0100

    Remove inline-blocks

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-133`,
+    css: `${CACHE_VERSION}-134`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
