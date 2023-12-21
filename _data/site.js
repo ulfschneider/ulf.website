3c91e947efa738bdd89121e12618b89e2680c090
ulfschneider <mail@ulfschneider.io>
2023-12-21 18:00:00 +0100

    Fix code for dark mode

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-21`,
-    css: `${CACHE_VERSION}-138`,
+    css: `${CACHE_VERSION}-139`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
