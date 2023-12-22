5eb727bb6872f988f4002b69412e0a81f9a0e3fe
ulfschneider <mail@ulfschneider.io>
2023-12-22 09:23:49 +0100

    Vertical spacing for blockquotes and github-style alerts

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-22`,
-    css: `${CACHE_VERSION}-139`,
+    css: `${CACHE_VERSION}-140`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
