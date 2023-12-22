055b1b4a2a7da1ce12406b39ba2aff7ccd7672b9
ulfschneider <mail@ulfschneider.io>
2023-12-22 13:27:49 +0100

    Do not use a bold font for h2 and h3

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-22`,
-    css: `${CACHE_VERSION}-144`,
+    css: `${CACHE_VERSION}-145`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
