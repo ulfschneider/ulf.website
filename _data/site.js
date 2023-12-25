8bfda9e32eaf833ef91bae4c585545fe0735c341
ulfschneider <mail@ulfschneider.io>
2023-12-25 16:32:43 +0100

    Style the post list

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-22`,
-    css: `${CACHE_VERSION}-147`,
+    css: `${CACHE_VERSION}-148`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
