54ab2f9b9c1f913621bd6ae3a8842ddbc6e96bc7
ulfschneider <mail@ulfschneider.io>
2023-12-25 17:11:53 +0100

    Fix list styling

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-22`,
-    css: `${CACHE_VERSION}-149`,
+    css: `${CACHE_VERSION}-150`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
