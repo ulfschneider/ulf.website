145e89a4d00dfb1ebd6bcb53c1d5c570c922bba5
ulfschneider <mail@ulfschneider.io>
2024-02-11 11:55:35 +0100

    Style site tags and footer

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-24`,
-    css: `${CACHE_VERSION}-162`,
+    css: `${CACHE_VERSION}-163`,
     json: `${CACHE_VERSION}-0`,
     search: `${CACHE_VERSION}-2`,
     image: `${CACHE_VERSION}-1`,
