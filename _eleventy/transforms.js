8b6454e51c4ababe48e448d5862a8fcf12f80980
ulfschneider <mail@ulfschneider.io>
2024-03-23 13:46:28 +0100

    Set defaults

--- _eleventy/transforms.js
+++ _eleventy/transforms.js
@@ -3,6 +3,12 @@ const Image = require("@11ty/eleventy-img");
 const site = require("../_data/site.js");
 const utils = require("../_eleventy/utils.js");
 
+const SITE_INPUT = site.input || "src";
+const SITE_OUTPUT = site.output || "_site";
+const IMAGE_FORMATS = site.images?.formats || ["webp", "auto"];
+const IMAGE_WIDTHS = site.images?.widths || [400, 900, 1280, "auto"];
+const IMAGE_SIZES = site.images?.sizes || "100vw";
+
 module.exports = {
   imageTransform: async function (content, contentPath) {
     const images = utils.allImageTags(content);
@@ -13,10 +19,10 @@ module.exports = {
       const { name, dir } = path.parse(src);
 
       try {
-        const imageMetaData = await Image(path.join(site.input, src), {
-          widths: site.images.widths,
-          formats: site.images.formats,
-          outputDir: path.join(site.output, dir, name),
+        const imageMetaData = await Image(path.join(SITE_INPUT, src), {
+          widths: IMAGE_WIDTHS,
+          formats: IMAGE_FORMATS,
+          outputDir: path.join(SITE_OUTPUT, dir, name),
           urlPath: path.join(dir, name),
           filenameFormat: function (hash, src, width, format, options) {
             return `${name}-${width}.${format}`;
@@ -29,7 +35,7 @@ module.exports = {
             loading: "lazy",
             decoding: "async",
             alt: alt || "",
-            sizes: site.images.sizes || "100vw",
+            sizes: IMAGE_SIZES,
           })
         );
       } catch (error) {
