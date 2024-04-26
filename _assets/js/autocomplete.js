4f162b78fc2613233d4e0fda8b7e1d9b9b0afedc
ulfschneider <mail@ulfschneider.io>
2024-04-26 08:08:35 +0200

    Debounce the search

--- _assets/js/autocomplete.js
+++ _assets/js/autocomplete.js
@@ -221,6 +221,22 @@ AutoComplete = (function () {
     }
   }
 
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
   function suggest({
     element,
     keyEvent,
@@ -350,7 +366,7 @@ AutoComplete = (function () {
       for (element of elements) {
         prepareElement({
           element: element,
-          queryData: queryData,
+          queryData: debounce(queryData, 300),
           data: [],
           threshold: threshold,
           onSelect: onSelect,
