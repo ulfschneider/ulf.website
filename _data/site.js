99b706d123af295dcd213f650aa6ca833c6fbf7e
ulfschneider <mail@ulfschneider.io>
2024-05-16 17:38:07 +0200

    Use the same baseline for outdent text and text

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-36`,
-    css: `${CACHE_VERSION}-188`,
+    css: `${CACHE_VERSION}-189`,
     json: `${CACHE_VERSION}-0`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-0`,
