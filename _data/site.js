243242cb55f33f8458e8a26018bd619f8e12e13a
ulfschneider <mail@ulfschneider.io>
2024-08-02 08:43:19 +0200

    Style site tags

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-37`,
-    css: `${CACHE_VERSION}-204`,
+    css: `${CACHE_VERSION}-205`,
     json: `${CACHE_VERSION}-0`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-0`,
