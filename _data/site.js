8bce9ec49fe41b5e4f3c68a74a115bb41aaadf95
ulfschneider <mail@ulfschneider.io>
2024-09-05 08:45:09 +0200

    Put a line of vertical space between the comments author and the body

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-38`,
-    css: `${CACHE_VERSION}-219`,
+    css: `${CACHE_VERSION}-220`,
     json: `${CACHE_VERSION}-0`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-0`,
