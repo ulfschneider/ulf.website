f89368c0e8a4fa30a3de12c22fbb6353e51b1918
ulfschneider <mail@ulfschneider.io>
2024-09-12 10:14:07 +0200

    Default leading

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-39`,
-    css: `${CACHE_VERSION}-224`,
+    css: `${CACHE_VERSION}-225`,
     json: `${CACHE_VERSION}-1`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-1`,
