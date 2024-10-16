1542f63f78faef32184a39e5c742a6f7a642b422
ulfschneider <mail@ulfschneider.io>
2024-10-16 18:24:23 +0200

    Offset for underlines

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-41`,
-    css: `${CACHE_VERSION}-231`,
+    css: `${CACHE_VERSION}-232`,
     json: `${CACHE_VERSION}-1`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-1`,
