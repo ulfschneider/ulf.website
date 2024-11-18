bdfbc65792b3ce315b17e232e9342b1738d1076a
ulfschneider <mail@ulfschneider.io>
2024-11-18 16:24:14 +0100

    Use the same font size for all headings

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-41`,
-    css: `${CACHE_VERSION}-240`,
+    css: `${CACHE_VERSION}-241`,
     json: `${CACHE_VERSION}-1`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-1`,
