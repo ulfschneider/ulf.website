c66fb90a91fe331af3e9ea31677c1d4f0ec0fcc2
ulfschneider <mail@ulfschneider.io>
2025-01-09 15:48:08 +0100

    Style y margin for blockquote, figure and table

--- _data/site.js
+++ _data/site.js
@@ -8,7 +8,7 @@ module.exports = {
   cacheVersioning: {
     runtime: `${CACHE_VERSION}-0`,
     script: `${CACHE_VERSION}-42`,
-    css: `${CACHE_VERSION}-253`,
+    css: `${CACHE_VERSION}-254`,
     json: `${CACHE_VERSION}-1`,
     image: `${CACHE_VERSION}-1`,
     font: `${CACHE_VERSION}-1`,
