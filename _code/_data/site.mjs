b26cd784098025f101a92936c4e46a7ebc179dc5
ulfschneider <mail@ulfschneider.io>
2025-02-11 09:39:19 +0100

    Read out 11ty server setting

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -1,20 +1,25 @@
 import "dotenv/config";
 import chalk from "chalk";
-const environment = process.env.DEV ? "DEV" : "PRD";
-const hostname = environment == "DEV" ? "localhost:8080" : "ulfschneider.io";
+
+const hostname =
+  process.env.ELEVENTY_RUN_MODE === "serve"
+    ? "localhost:8080"
+    : "ulfschneider.io";
 const origin =
-  environment == "DEV" ? `http://${hostname}` : `http://${hostname}`;
+  process.env.ELEVENTY_RUN_MODE === "serve"
+    ? `http://${hostname}`
+    : `https://${hostname}`;
 const buildTime = new Date();
 
 function deriveVersion(version) {
-  return environment == "DEV" ? "" : `-${version}`;
+  return process.env.ELEVENTY_RUN_MODE === "serve" ? "" : `-${version}`;
 }
 
 function useServiceWorker() {
-  if (environment == "DEV") {
+  if (process.env.SERVICE_WORKER == "false") {
     console.log(
       chalk.yellow(
-        "Not registering service worker because of local dev environment",
+        "Not registering service worker because of environment variable setting SERVICE_WORKER=false",
       ),
     );
     return false;
@@ -23,10 +28,10 @@ function useServiceWorker() {
 }
 
 function getGitCommitDates() {
-  if (environment == "DEV") {
+  if (process.env.GIT_COMMIT_DATES == "false") {
     console.log(
       chalk.yellow(
-        "Not getting git commit dates because of local dev environment",
+        "Not getting git commit dates because of environment variable setting GIT_COMMIT_DATES=false",
       ),
     );
     return false;
@@ -40,7 +45,6 @@ export default {
   title: "Ulf Schneider – Developer and Agile Coach",
   description:
     "Hi, I´m Ulf Schneider, a software developer and agile coach living in Paderborn, Germany.",
-  environment: environment,
   name: hostname,
   hostname: hostname,
   origin: origin,
