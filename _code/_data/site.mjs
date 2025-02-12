d1c97d64b71cabaab5c90484ab174038752fd729
ulfschneider <mail@ulfschneider.io>
2025-02-12 14:56:41 +0100

    Style table

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -64,7 +64,7 @@ export default {
       json: getVersion(2),
       image: getVersion(1),
       font: getVersion(1),
-      css: getVersion(7),
+      css: getVersion(8),
     },
   },
   ownership: {
