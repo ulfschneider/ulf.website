335e8d87bd906d356b38e28f06608c772f6f9d66
ulfschneider <mail@ulfschneider.io>
2025-02-12 14:18:50 +0100

    Set scroll margin for all possible scroll targets

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -64,7 +64,7 @@ export default {
       json: getVersion(2),
       image: getVersion(1),
       font: getVersion(1),
-      css: getVersion(6),
+      css: getVersion(7),
     },
   },
   ownership: {
