22aac48c25eeab977a422990c402cf2b27e0e864
ulfschneider <mail@ulfschneider.io>
2025-02-17 07:19:14 +0100

    Renew css cache

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -64,7 +64,7 @@ export default {
       json: getVersion(2),
       image: getVersion(1),
       font: getVersion(1),
-      css: getVersion(15),
+      css: getVersion(16),
     },
   },
   ownership: {
