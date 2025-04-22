709dd59090cd2f12b3b450660ddba885f2d42b37
ulfschneider <mail@ulfschneider.io>
2025-04-22 09:12:36 +0200

    Renew the cache

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -7,7 +7,7 @@ const HTML_VERSION = 2;
 const JSON_VERSION = 2;
 const IMAGE_VERSION = 2;
 const FONT_VERSION = 1;
-const CSS_VERSION = 36;
+const CSS_VERSION = 37;
 
 const hostname =
   process.env.ELEVENTY_RUN_MODE === "serve"
