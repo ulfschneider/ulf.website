5e96d5a157f96162c006086cd9bbc68987ea6dee
ulfschneider <mail@ulfschneider.io>
2025-09-12 13:52:00 +0200

    Use aspect-square

--- _code/_data/site.mjs
+++ _code/_data/site.mjs
@@ -7,7 +7,7 @@ const HTML_VERSION = 3;
 const JSON_VERSION = 3;
 const IMAGE_VERSION = 2;
 const FONT_VERSION = 1;
-const CSS_VERSION = 78;
+const CSS_VERSION = 79;
 
 const hostname =
   process.env.ELEVENTY_RUN_MODE === "serve"
